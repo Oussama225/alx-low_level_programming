43f2b051018ddbb2dd3cc2a1a3572de174c195e1
Oussama225 <oussamabouraji590@gmail.com>
2023-06-16 19:04:19 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,14 @@
  *     */
 int main(void)
 {
-		char c = 'a';
+		char c = 'a'; */ 
 
-			while (c <= 'z')
+			while (c <= 'z') */ 
 					{
-								putchar(c);
-										c++;
+								putchar(c); */ 
+										c++; */ 
 											}
-				putchar('\n');
+				putchar('\n'); */ 
 
-					return (0);
+					return (0); */ 
 }
