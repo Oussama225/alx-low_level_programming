b566f84416f12ba20a9617f119ea68183b76aa50
Oussama225 <oussamabouraji590@gmail.com>
2023-06-19 14:15:09 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,25 +18,25 @@ int i, j, k;
 i = 0;
 
 while (i < 100)
-{
+						{
 j = i % 10; /* singles digit */
 k = i / 10; /* doubles digit */
 
 if (k < j)
 {
-putchar(k + '0'); 
-putchar(j + '0');																			
+putchar(k + '0');
+																					putchar(j + '0');
 
-if (i < 89)
+																					 if (i < 89)
 																					{
-																					putchar(44);
-																					putchar(32);
+																					 putchar(44);
+																					 putchar(32);
 																																										}
-																					}
+																												}
 
-i++;
+ i++;
 }
 putchar('\n');
 
-return (0);
+  return (0);
 }
