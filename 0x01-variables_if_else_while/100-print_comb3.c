7b6dff2af11faead8f9fd1639d94309529dac488
Oussama225 <oussamabouraji590@gmail.com>
2023-06-19 14:24:03 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - main block
+ * main - Entry point
+ *
  * Description: Print all possible combinations of two digits.
  * Numbers must be separated by commas and a space.
  * 01 and 10 are considered as the same combination of the two digits.
@@ -9,6 +11,7 @@
  * You can only use `putchar`.
  * You can only use `putchar` up to 5 times.
  * You are not allowed to use any variable of type `char`.
+ *
  * Return: 0
  */
 int main(void)
@@ -18,7 +21,7 @@ int i, j, k;
 i = 0;
 
 while (i < 100)
-						{
+{
 j = i % 10; /* singles digit */
 k = i / 10; /* doubles digit */
 
@@ -27,16 +30,17 @@ if (k < j)
 putchar(k + '0');
 																					putchar(j + '0');
 
-																					 if (i < 89)
+																					if (i < 89)
 																					{
-																					 putchar(44);
-																					 putchar(32);
+																					putchar(44);
+																					putchar(32);
 																																										}
 																												}
 
- i++;
+i++;
 }
 putchar('\n');
 
-  return (0);
+return (0);
 }
+
