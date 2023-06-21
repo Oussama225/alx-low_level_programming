050cba9578c26ce7dca76aed31ee390fd9a2095d
Oussama225 <oussamabouraji590@gmail.com>
2023-06-20 17:21:57 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,21 +20,19 @@ for (mult = 1; mult <= 9; ++mult)
 {
 _putchar(',');
 _putchar(' ');
+
 prod = num * mult;
-
-/*
- * put space if product is a single number
+ /*
+ * put space if product is a single number                                                                              
  * place the first digit if its two numbers
- */																					
-
+ */
 if (prod <= 9)
-
 _putchar(' ');
-																					else
-																					_putchar((prod / 10) + 48); /*get the first digit*/
+else
+_putchar((prod / 10) + 48); /*get the first digit*/
 
-																					_putchar((prod % 10) + 48); /*get the second digit*/
-																					}
+_putchar((prod % 10) + 48); /*get the second digit*/
+}
 _putchar('\n');
 }
 }
